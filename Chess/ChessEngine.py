d5863226e1a2b2e7211c74e538ee5415454f4e86
Shiwarne Silva <sshiwarne@gmail.com>
2024-03-23 03:36:17 +0530

    conflicts resolved

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -35,6 +35,40 @@ class GameState():
             self.whiteToMove = not self.whiteToMove  # Switch back to the previous player's turn
 
 
+    '''
+    All moves considering checks
+    '''
+    def getValidMoves(self):
+        return self.getAllPossibleMoves()
+
+    '''
+    All moves withput considering checks
+    '''
+    def getAllPossibleMoves(self):
+        moves = []
+        for r in range(len(self.board)): #number of rows
+            for c in range(len(self.board[r])): #number of cols in given row
+                turn = self.board[r][c][0]
+                if (turn == 'w' and self.whiteToMove) and (turn == 'b' and not self.whiteToMove):
+                    piece = self.board[r][c][1]
+                    if piece == 'p':
+                        self.getPawnMoves(r, c, moves)
+                    elif piece == 'R':
+                        self.getRookMoves(r, c, moves)
+        return moves
+    '''
+    Get all the pawn moves for the pawn located at row, col and add these moves to the list
+    '''
+    def getPawnMoves(self, r, c, moves):
+        pass
+
+    '''
+        Get all the rook moves for the rook located at row, col and add these moves to the list
+    '''
+
+    def getRookMoves(self, r, c, moves):
+        pass
+
 class Move():
     # maps keys to values
     # key : value
