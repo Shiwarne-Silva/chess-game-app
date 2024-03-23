8d27f3d1ad3137c0789c9677f6bbe565f14e81d8
Shiwarne Silva <sshiwarne@gmail.com>
2024-03-23 21:26:43 +0530

    pawn moves partially implemented

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -1,16 +1,16 @@
 """
 This class is responsible for storing all the information about the current state of the chess game. it will also be
-responsible for determing
+responsible for determining
 """
-class GameState():
+class GameState:
     def __init__(self):
         self.board = [
             ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
             ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "wp", "--", "--", "--"],
             ["--", "--", "--", "--", "--", "--", "--", "--"],
             ["--", "--", "--", "--", "--", "--", "--", "--"],
-            ["--", "--", "--", "--", "--", "--", "--", "--"],
-            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "bp", "--", "--", "--", "--", "--"],
             ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
         ]
@@ -42,25 +42,47 @@ class GameState():
         return self.getAllPossibleMoves()
 
     '''
-    All moves withput considering checks
+    All moves without considering checks
     '''
     def getAllPossibleMoves(self):
-        moves = [Move((6, 4), (4, 4), self.board)]
+        moves = []
         for r in range(len(self.board)): #number of rows
             for c in range(len(self.board[r])): #number of cols in given row
                 turn = self.board[r][c][0]
-                if (turn == 'w' and self.whiteToMove) and (turn == 'b' and not self.whiteToMove):
+                if (turn == 'w' and self.whiteToMove) or (turn == 'b' and not self.whiteToMove):
                     piece = self.board[r][c][1]
                     if piece == 'p':
                         self.getPawnMoves(r, c, moves)
                     elif piece == 'R':
                         self.getRookMoves(r, c, moves)
         return moves
+
     '''
     Get all the pawn moves for the pawn located at row, col and add these moves to the list
     '''
     def getPawnMoves(self, r, c, moves):
-        pass
+        if self.whiteToMove: #white pawn moves
+            if self.board[r-1][c] == "--": #1 square pawn advance
+                moves.append(Move((r, c), (r-1, c), self.board))
+                if r == 6 and self.board[r-2][c] == "--": #2 square pawn advance
+                    moves.append(Move((r, c), (r-2, c), self.board))
+            if c-1 >= 0: #captures to the left
+                if self.board[r-1][c-1][0] == 'b': #enemy piece to capture to the left
+                    moves.append(Move((r, c), (r-1, c-1), self.board))
+            if c+1 <= 7: #captures to the right
+                if self.board[r-1][c+1][0] == 'b': #enemy piece to capture to the right
+                    moves.append(Move((r, c), (r-1, c+1), self.board))
+        else: #black pawn moves
+            if self.board[r+1][c] == "--":
+                moves.append(Move((r, c), (r+1, c), self.board))
+                if r == 1 and self.board[r+2][c] == "--":
+                    moves.append(Move((r, c), (r+2, c), self.board))
+            if c-1 >= 0: #captures to the left
+                if self.board[r+1][c-1][0] == 'w': #enemy piece to capture to the left
+                    moves.append(Move((r, c), (r+1, c-1), self.board))
+            if c+1 <= 7: #captures to the right
+                if self.board[r+1][c+1][0] == 'w': #enemy piece to capture to the right
+                    moves.append(Move((r, c), (r+1, c+1), self.board))
 
     '''
         Get all the rook moves for the rook located at row, col and add these moves to the list
@@ -87,10 +109,9 @@ class Move():
         self.pieceMoved = board[self.startRow][self.startCol]
         self.pieceCaptured = board[self.endRow][self.endCol]
         self.moveID = self.startRow * 1000 + self.startCol * 100 + self.endRow * 10 + self.endCol
-        print(self.moveID)
 
     '''
-    Overiding the equals method
+    Overriding the equals method
     '''
     def __eq__(self, other):
         if isinstance(other, Move):
